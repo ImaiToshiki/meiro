b105ab31b889a65d3df564d152f83256419aaf3f
Toshiki Imai <tosiki_801_life@outlook.jp>
2019-08-13 21:46:57 +0900

    class and fuction

--- meiro_oth.py
+++ meiro_oth.py
@@ -1,16 +1,15 @@
 import random
 
 class Meiro():
-
     WALL = 1
     ROOT = 0
 
     def __init__(self, field_x, field_y, mode):
-        self.field_x = field_x  # 奇数
-        self.field_y = field_y  # 奇数
+        self.field_x = field_x
+        self.field_y = field_y
         self.field = []
         self.mode = mode  # 棒倒し法の場合は１、穴掘り法は２、それ以外は０
-        #迷路は5*5以上、奇数
+        # 迷路は5*5以上、奇数
         if self.field_x < 5 or self.field_y < 5:
             print('大きさは５*５以上')
             exit()
@@ -23,21 +22,20 @@ class Meiro():
             print('正しいモードを選択(0)')
             exit()
 
-
     # 迷路の枠を生成
     def meiro_frame(self):
         if self.mode == 0:
             for i in range(self.field_y):
                 row = []
                 for j in range(self.field_x):
-                    if i == 0 or i == self.field_y-1 or j == 0 or j == self.field_x-1:    #上下枠
+                    if i == 0 or i == self.field_y - 1 or j == 0 or j == self.field_x - 1:  # 上下枠
                         sell = self.WALL
                     else:
                         sell = self.ROOT
                     row.append(sell)
                 self.field.append(row)
         self.field[1][1] = 'S'
-        self.field[self.field_y-2][self.field_x-2] = 'G'
+        self.field[self.field_y - 2][self.field_x - 2] = 'G'
 
     # 迷路を表示
     def meiro_show(self):
@@ -52,6 +50,7 @@ class Meiro():
                 elif self.field[i][j] == 'G':
                     print('GOL', end="")
             print()
+        print()
 
     # 迷路生成(棒倒し法)
     def meiro_make_stick(self):
@@ -62,12 +61,23 @@ class Meiro():
         for i in range(self.field_y):
             for j in range(self.field_x):
                 # 1マス置きに倒す方向を考える
-                if j > 0 and j < self.field_x-1 and j % 2 == 0 and i > 1 and i < self.field_y-2 and i % 2 == 0:
+                if j > 0 and j < self.field_x - 1 and j % 2 == 0 and i > 1 and i < self.field_y - 2 and i % 2 == 0:
                     while True:
                         direction = random.randint(0, 3)
-                        if self.field[i + y_dir[direction]][j + x_dir[direction]] == self.WALL or (i > 2 and direction == 3):
+                        if self.field[i + y_dir[direction]][j + x_dir[direction]] == self.WALL or (
+                                i > 2 and direction == 3):
                             continue
                         else:
                             self.field[i + y_dir[direction]][j + x_dir[direction]] = self.WALL
                             self.field[i][j] = self.WALL
                             break
+
+# 与えられたモードで与えられた個数迷路を生成する
+def meiro_generator(width, height, mode, count):
+    meiros = []
+    for i in range(count):
+        meiro = Meiro(width, height, mode)
+        meiro.meiro_frame()
+        if mode == 0:
+            meiro.meiro_make_stick()
+        meiro.meiro_show()
